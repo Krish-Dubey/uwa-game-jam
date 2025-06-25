f0ad7f134a6477d9af13f2e3961e60fd791be4ba
zuhalhabibi <71216450+lowestpc@users.noreply.github.com>
2025-06-25 13:39:51 +0700

    Basic Pathfinding Implementation
    Added Walkertest as the Agent
    Spawn Node as the spawner
    EndPath node as the navigation target

---
+++ scripts/walkertest.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+
+var navigation_target = Vector2.ZERO #Initial Target
+@export var movement_speed: float = 50
+
+func _physics_process(delta: float) -> void:
+	navigation_agent_2d.target_position = navigation_target
+	var current_agent_position = global_position
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	var new_velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	velocity = new_velocity
+	move_and_slide()
+	
+	pass	
