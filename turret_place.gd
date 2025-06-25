2f2a0ed8120f35b0a1613fb25e9f3c766e3f2ac3
zuhalhabibi <71216450+lowestpc@users.noreply.github.com>
2025-06-25 23:01:22 +0700

    Turret Place fix

--- turret_place.gd
+++ turret_place.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 @export var tile_map : TileMapLayer
+@export var road_tile_map : TileMapLayer
 
 func _input(event):
 	if Input.is_action_just_pressed("left_click"):
 		var mouse_pos = get_global_mouse_position()
 		var tile_mouse_pos = tile_map.local_to_map(mouse_pos)
-		print(tile_mouse_pos)
-		tile_map.set_cell(tile_mouse_pos, 2, Vector2i(0,0), 1)
+		
+		var road_tile_map = road_tile_map.get_cell_source_id(road_tile_map.local_to_map(mouse_pos))
+		
+		if road_tile_map == 1:
+			print("You cant place on a road")
+		else:
+			print(tile_mouse_pos)
+			tile_map.set_cell(tile_mouse_pos, 2, Vector2i(0,0), 1)
 		
