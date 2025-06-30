322d49db69af7a1336fa5212c9ace0d812846126
Wxvxl <metal123cool1115@gmail.com>
2025-06-30 23:29:14 +0800

    FINAL PUSH

--- scripts/home_base.gd
+++ scripts/home_base.gd
@@ -3,12 +3,11 @@ extends Area2D
 var health = 20
 var maxhealth = 20
 @export var health_bar : ProgressBar
-
 @export var game_over_screen : PackedScene
 
 func _on_area_entered(area: Area2D) -> void:
 	health -= area.get_parent().base_damage
-	health_bar.value = (health / maxhealth) * 100
+	health_bar.value = ((float(health) / float(maxhealth)) * 100)
 	if health == 0:
 		var game_over = game_over_screen.instantiate()
 		get_tree().current_scene.add_child(game_over)
